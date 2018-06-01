2eff1bdc69b0c04d4fe3c6722f93c881a688e459
Matheus Rodrigues <matheus.etb@gmail.com>
2018-06-01 20:14:42 -0300

    Sexto commit

--- src/br/com/farmacia/teste/Teste.java
+++ src/br/com/farmacia/teste/Teste.java
@@ -1,20 +1,39 @@
 package br.com.farmacia.teste;
 
 import java.lang.reflect.Field;
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 import br.com.farmacia.modelo.Categoria;
-import br.com.farmacia.modelo.TipoPagamento;
-import br.com.farmacia.modelo.pagamento.PagamentoEstrategia;
 
 public class Teste {
-	public static void main(String[] args) {
-		PagamentoEstrategia pagamentoEstrategia = TipoPagamento.BOLETO.realizarPagamento();
-		String pagamento = pagamentoEstrategia.pagar(new BigDecimal(50));
-		System.out.println(pagamento);
-		
-		Field[] campos = Categoria.GripeResfriado.class.getDeclaredFields();
-		for(Field field : campos)
-			System.out.println(field.getName());
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
+		Map<String, List<String>> campos = new HashMap<>();
+
+		Class<?>[] camposCategoria = Categoria.class.getDeclaredClasses();
+		List<String> subCampos = null;
+
+		for (Class<?> campo : camposCategoria) {
+			campos.put(campo.getSimpleName(), null);
+			subCampos = new ArrayList<>();
+			for (Field field : campo.getDeclaredFields()) {
+				subCampos.add(field.get(field).toString());
+			}
+			campos.putIfAbsent(campo.getSimpleName(), subCampos);
+		}
+
+		ResourceBundle bundle = ResourceBundle.getBundle("br.com.farmacia.modelo.categorias");
+		Enumeration<String> chaves = bundle.getKeys();
+		while (chaves.hasMoreElements()) {
+			String chave = chaves.nextElement();
+			List<String> lista = campos.remove(chave);
+			campos.put(bundle.getString(chave), lista);
+		}
+
+		System.out.println(campos);
 	}
 }
